2d14de0729de98e031b4ba59bf6c8c352843babe
Yacin Bahi <yacin@red64.io>
2022-11-09 20:30:42 +0100

    skip rate limit if not set

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,15 +1,17 @@
 const rateLimiter = require("express-rate-limit");
 
 module.exports = (config) => (req, res, next) => {
-  rateLimiter({
-    windowMs: config["rate-limiter"]["windowsMs"] || 10000, 
-    max: config["rate-limiter"]["max"] || 60, 
-    keyGenerator: (req) => {
-      const ip =
-        req.headers["x-real-ip"] ||
-        req.headers["x-forwarded-for"] ||
-        req.connection.remoteAddress;
-      return ip;
-    },
+  if (config['rateLimiter']) {
+    rateLimiter({
+      windowMs: config["rateLimiter"]["windowsMs"] || 10000, 
+      max: config["rateLimiter"]["max"] || 60, 
+      keyGenerator: (req) => {
+        const ip =
+          req.headers["x-real-ip"] ||
+          req.headers["x-forwarded-for"] ||
+          req.connection.remoteAddress;
+        return ip;
+      },
+    })
   }
-)};
+};
